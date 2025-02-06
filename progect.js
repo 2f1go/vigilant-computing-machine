9bb5fb2e1f38c01859e7faa26d498e0216df852a
2f1go <players45612@gmail.com>
2025-02-06 11:06:32 +0900

    Add files via upload

---
+++ progect.js
@@ -0,0 +1,43 @@
+function red(){
+    document.getElementById("block").style.backgroundColor="red";
+}
+function green(){
+    document.getElementById("block").style.backgroundColor = "green";
+}
+function blue(){
+    document.getElementById("block").style.backgroundColor = "blue";
+}
+function yellow(){
+    document.getElementById("block").style.backgroundColor = "yellow";
+}
+function organ(){
+    document.getElementById("block").style.backgroundColor = "orange";
+}
+function lightblue(){
+    document.getElementById("block").style.backgroundColor = "lightblue";
+}
+function blackman(){
+    document.getElementById("block").style.backgroundColor = "black";
+}
+function poop(){
+    document.getElementById("block").style.backgroundColor = "rgb(111, 74, 1)";
+}
+function whiteman(){
+    document.getElementById("block").style.backgroundColor = "white";
+}
+function violetta(){
+    document.getElementById("block").style.backgroundColor = "rgb(95, 16, 95)";
+}
+function darkgreench(){
+    document.getElementById("block").style.backgroundColor = "darkgreen";
+}
+function borodino(){
+    document.getElementById("block").style.backgroundColor = "brown";
+}
+function clCol(){
+    document.getElementById("block").style.backgroundColor = "white";
+}
+function amoGus(){
+    var amgus=document.body;
+    amgus.style.backgroundImage='url("previewfile_3288091243.jpg")';
+}
